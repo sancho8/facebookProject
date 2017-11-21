bcd7d276740b57780a9ed64e2aa27cd01c7bcef4
Oleksandr <yaroshenko.aleksandr8@gmail.com>
2017-11-21 02:10:26 +0200

    added places api

--- facebook-app/app/js/app.js
+++ facebook-app/app/js/app.js
@@ -9,13 +9,19 @@ $scope.accessToken = "";
 
 $scope.eventsData = [];
 
+$scope.isLoggedIn = false;
+
+$scope.addressComponents = "adddress";
+
+$scope.locationDetails = {};
+
 $scope.executeSearch = function(){
 	$http({
 		method: "GET",
 		url: "http://localhost:8080/events",
 		params: {
-			lat: 40.710803,
-			lng: -73.964040,
+			lat: $scope.locationDetails[$scope.locationDetails.length-2],
+			lng: $scope.locationDetails[$scope.locationDetails.length-1],
 			distance: 100,
 			sort: "venue",
 			accessToken: $scope.accessToken
@@ -44,7 +50,10 @@ $scope.myFacebookLogin = function() {
 		if (response.authResponse) {
 			var access_token =   FB.getAuthResponse()['accessToken'];
 			console.log('Access Token = '+ access_token);
+			debugger;
+			$scope.isLoggedIn = true;
 			$scope.accessToken = access_token;
+			$('#loginBtn').click();
 			FB.api('/me', function(response) {
 				console.log('Good to see you, ' + response.name + '.');
 			});
@@ -58,3 +67,51 @@ $scope.myFacebookLogin = function() {
 	} 
 
 })
+.directive('googleplace', [ function() {
+		return {
+			require: 'ngModel',
+			scope: {
+				ngModel: '=',
+				details: '=?'
+			},
+			link: function($scope, element, attrs, model) {
+				var options = {
+					types: [],
+					componentRestrictions: {country: "ua"}
+				};
+
+				$scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+				google.maps.event.addListener($scope.gPlace, 'place_changed', function() {
+					var geoComponents = $scope.gPlace.getPlace();
+					var latitude = geoComponents.geometry.location.lat();
+					var longitude = geoComponents.geometry.location.lng();
+					var addressComponents = geoComponents.address_components;
+
+					addressComponents = addressComponents.filter(function(component){
+						switch (component.types[0]) {
+                        case "locality": // city
+                        return true;
+                        case "administrative_area_level_1": // state
+                        return true;
+                        case "country": // country
+                        return true;
+                        default:
+                        	return false;
+                        }
+                    }).map(function(obj) {
+                    	return obj.long_name;
+                    });
+
+                    addressComponents.push(latitude, longitude);
+
+                    $scope.$apply(function() {
+                    $scope.details = addressComponents; // array containing each location component
+					console.log(addressComponents);
+					$scope.addressComponents = addressComponents;
+                    model.$setViewValue(element.val());
+                });
+                });
+			}
+		};
+	}]);
